60449a81c5f829f5ec7ada0c4aa24a0e09b35f76
476MHz <kabob8933@gmail.com>
2020-09-23 22:13:39 +0100

    add recovery bot - stage1

---
+++ recovery.py
@@ -0,0 +1,23 @@
+print('init')
+import discord
+import subprocess
+print('stage0')
+import asyncio 
+print('stage1')
+from discord.ext import commands
+print('stage2')
+client = commands.Bot(command_prefix = '.')
+print('botStage')
+
+tokenfile = open("/home/pi/Sans/recoverytoken", "r")
+token = tokenfile.read()
+tokenfile.close()
+
+@client.event
+async def on_ready():
+    print('Ready.')
+
+@client.command()
+async def recover(ctx):
+    await ctx.send("Performing recovery. Please make sure the latest git commit is in a working state.")
+    subprocess.run(['sudo', '/home/pi/duckdns/sans.sh'])
