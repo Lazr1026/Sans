e8d753b2243766c58b9b885ab9f3708e5ab97d45
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-09-23 19:25:45 -0700

    fucking aaaaa im done for today

--- sans.py
+++ sans.py
@@ -259,16 +259,16 @@ async def help(ctx):
         await ctx.send('```Memes are: radeon, snas, uwuham, lazr```')
         sent = 1
     if ctx.message.content == ".help radeon":
-        await ctx.send('```Type the command``` ;)')
+        await ctx.send('Type the command ;)')
         sent = 1
     if ctx.message.content == ".help snas":
-        await ctx.send('```Type the command``` ;)')
+        await ctx.send('Type the command ;)')
         sent = 1
     if ctx.message.content == ".help uwuham":
-        await ctx.send('```Type the command``` ;)')
+        await ctx.send('Type the command ;)')
         sent = 1
     if ctx.message.content == ".help lazr":
-        await ctx.send('```Type the command``` ;)')
+        await ctx.send('Type the command ;)')
         sent = 1
     if sent == 0:
         await ctx.send('```What can I help you with?\n\n assistnce\n invite\n memes```')
