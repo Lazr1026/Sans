cd3414937007817b9326758cae912659b72ec396
476MHz <kabob8933@gmail.com>
2020-10-02 03:47:58 +0100

    fix broken thing

--- sans.py
+++ sans.py
@@ -393,6 +393,7 @@ async def vc(ctx):
         sent = 1
     if ctx.message.content == ".vc wiiu":
         await ctx.send('You can play classics on your wiiu with this tool: https://gbatemp.net/threads/release-uwuvci-injectiine.486781/ \nUsage guide here: https://flumpster.github.io/instructions/index')
+        sent = 1
     if sent == 0:
         await ctx.send('Invalid syntax. Options are: 3ds, wiiu.')
 
