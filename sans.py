427bf2714203d14ac5659167b2991b7ecd48317d
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-02 16:10:51 -0700

    twlfix pt.5

--- sans.py
+++ sans.py
@@ -163,13 +163,13 @@ async def twlmenu(ctx):
 async def twlfix(ctx):
     sent = 0
     if str(ctx.message.content).startswith(".twlfix cfw"):
-        await ctx.send('```Use this tool to fix the dsiware on your 3ds!``` <https://github.com/MechanicalDragon0687/TWLFix-CFW/releases/>')
+        await ctx.send('```Use this tool to fix the dsiware on your 3ds!``` <https://github.com/MechanicalDragon0687/TWLFix-CFW/releases/>, please note that you must do a system update after the process has finished')
         sent = 1
     if str(ctx.message.content).startswith(".twlfix hbl"):
-        await ctx.send('```Use this tool to fix the dsiware on your 3ds!``` <https://github.com/MechanicalDragon0687/TWLFix-3DS/releases/')
+        await ctx.send('```Use this tool to fix the dsiware on your 3ds!``` <https://github.com/MechanicalDragon0687/TWLFix-3DS/releases/, please note that you must do a system update after the process has finished')
         sent = 1
     if str(ctx.message.content).startswith(".twlfix stock"):
-        await ctx.send('```Use this tool to fix the dsiware on your 3ds!``` <https://github.com/MechanicalDragon0687/TWLFix/wiki/Instructions>')
+        await ctx.send('```Use this tool to fix the dsiware on your 3ds!``` <https://github.com/MechanicalDragon0687/TWLFix/wiki/Instructions>, please note that you must do a system update after the process has finished')
         sent = 1
     if sent == 0:
         await ctx.send('```What twlfix would ya like? cfw, 3ds, stock```')
