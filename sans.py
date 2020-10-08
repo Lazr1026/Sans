86f22cab796be747c14a5dd98731e8c7adfecb5f
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-08 14:08:03 -0700

    yarr harr pt.2

--- sans.py
+++ sans.py
@@ -106,6 +106,7 @@ async def soundhax(ctx):
         
 @client.command()
 async def troubleshoot(ctx):
+        """troubleshoot"""
         embed = discord.Embed(title="troubleshoot", color.discord.Color.purple())
         embed.url = "https://3ds.eiphax.tech/issues.html"
         embed.description = "A simple troubleshooting guide for common CFW and homebrew installation issues when using popular recent 3DS hacking methods."
