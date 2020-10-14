de245fb0c0a8b1d0fd258724c2752ea0e157a55f
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-13 19:45:15 -0700

    BEEEEEAAAAAANNNNNN\

--- sans.py
+++ sans.py
@@ -420,9 +420,9 @@ async def ban (ctx, member:discord.User=None, reason =None):
     if reason == None:
         reason = "[no reason specified]"
     message = f"You have been banned from {ctx.guild.name} for {reason}"
-    await member.send(message)
-    await ctx.guild.ban(member, reason=reason)
+    await ctx.guild.ban(member, reason=reason, delete_message_days=0)
     await ctx.channel.send(f"{member} is now banned.")
+    await member.send(message)
 @client.command()
 @commands.has_any_role('Owner', 'Owner 🍙', 'Staff', 'Admin👮‍', 'Helper')
 async def kick(ctx, member : discord.Member, *,reason=0):
