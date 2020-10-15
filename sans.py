3522c01dfb8de7a3b6b372f8a58a3d0be8a970a7
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-15 10:04:18 -0700

    lumabug is an embed now

--- sans.py
+++ sans.py
@@ -336,7 +336,9 @@ async def cfwuses(ctx):
 
 @client.command()
 async def notbricked(ctx):
-    await ctx.send('No, you are not bricked if your power LED turns on and off after you installed b9s, you are not bricked and are just missing a file called boot.firm in the root of your SD card to fix the issue\n 1. Check you inserted the SD card in your console\n 2. Place/replace the file, downloading it from <https://github.com/LumaTeam/Luma3DS/releases>')
+        embed = discord.Embed(title="No you are not bricked")
+        embed.description = "if your power LED turns on and off after you installed b9s, you are not bricked and are just missing a file called boot.firm in the root of your SD card to fix the issue\n 1. Check you inserted the SD card in your console\n 2. Place/replace the file, downloading it from [here](https://github.com/LumaTeam/Luma3DS/releases)."
+        await ctx.send(embed=embed)
 
 @client.command()
 async def lumabug(ctx):
