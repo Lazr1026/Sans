7c73ef10f5005035b21dfebf184b010be1a29166
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-16 20:27:20 -0700

    bean update

--- sans.py
+++ sans.py
@@ -423,7 +423,7 @@ async def ban (ctx, member:discord.User=None, reason =None):
         reason = "[no reason specified]"
     message = f"You have been banned from {ctx.guild.name} for {reason}"
     await ctx.guild.ban(member, reason=reason, delete_message_days=0)
-    await ctx.channel.send(f"{member} is now banned, for reason {reason}.")
+    await ctx.channel.send(f"{member} has been b&. 👍")
     await member.send(message)
 @client.command()
 @commands.has_any_role('Owner', 'Staff', 'Admin', 'Helper')
