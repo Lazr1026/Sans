a4dd3595f77e846bebf45f2ee8e8b933405ebca6
476MHz <kabob8933@gmail.com>
2020-10-20 13:27:22 +0100

    fix more potential ping issues

--- sans.py
+++ sans.py
@@ -541,8 +541,9 @@ async def proxyuser(ctx, user: Union[discord.Member, discord.User, int, str], *,
     else:
         name = user.display_name
         avatar = user.avatar_url
-    if message == "@everyone" or message == "@here":
-        message = "everyone"
+    if "@everyone" in message or "@here" in message:
+        message = message.replace("@everyone", "everyone")
+        message = message.replace("@here", "here")
     webhooks = await ctx.channel.webhooks()
     if len(webhooks) == 0:
         webhook = await ctx.channel.create_webhook(name="Temp Webhook For -sudo")
