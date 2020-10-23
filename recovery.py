7444239cddab6e53cfd71529e1944648e92456c9
476MHz <kabob8933@gmail.com>
2020-10-23 13:34:00 +0100

    clean up recovery.py

--- recovery.py
+++ recovery.py
@@ -1,32 +1,29 @@
-print('init')
+print('Loading...')
 import discord
 import subprocess
 from os import system
-print('stage0')
 import asyncio 
-print('stage1')
 from discord.ext import commands
-print('stage2')
-client = commands.Bot(command_prefix = '.')
-print('botStage')
+bot = commands.Bot(command_prefix = '.')
 
-tokenfile = open("/home/pi/Sans/recoverytoken", "r")
-token = tokenfile.read()
-tokenfile.close()
+tokendir = os.path.dirname(os.path.realpath(__file__))
+with open(tokendir + "/recoverytoken") as tokenfile:
+    token = tokenfile.read()
 
-@client.event
+@bot.event
 async def on_ready():
     print('Ready.')
 
-client.remove_command('help')
+bot.remove_command('help')
 
 @commands.has_any_role('Owner')
-@client.command()
+@bot.command()
 async def recover(ctx):
     await ctx.send("Performing recovery. Please make sure the latest git commit is in a working state.")
     subprocess.run(['sudo', '/home/pi/duckdns/sans.sh'])
+
 @commands.has_any_role('Owner')
-@client.command()
+@bot.command()
 async def showerr(ctx):
     await ctx.send("Please wait while the error status is obtained. This will take around 10 seconds.")
     cmd = "bash /home/pi/duckdns/err.sh"
@@ -38,8 +35,9 @@ async def showerr(ctx):
     system(cmd)
     err = "Error (or command output): \n" + "```" + err + "```"
     await ctx.send(err)
-@client.command()
+
+@bot.command()
 async def code(ctx):
     await ctx.send("https://github.com/Lazr1026/Sans")
 
-client.run(token)
+bot.run(token)
