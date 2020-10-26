36c71465f5af7f6d9c5c5beef7a36399643dcc8e
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-25 19:15:33 -0700

    cbhc rules embed

--- sans.py
+++ sans.py
@@ -467,17 +467,20 @@ async def meta(ctx):
 @bot.command()
 async def cbhc(ctx):
     '''Show some CBHC rules that should be followed'''
-    await ctx.send(cleandoc("""
-        CBHC Rules:
-        - The DS game has to be legitimately installed from the eShop!
-        - Don’t format the system while CBHC is installed!
-        - Don’t delete the user account that bought the DS VC game!
-        - Don’t re-install the same game using WUP Installer or from the eShop!
-        - Don’t install Haxchi over CBHC! (You will not brick, but it will cause a boot-loop! Hold A when booting to access the Homebrew Launcher and uninstall CBHC.)
-        - Don’t uninstall the DS Virtual Console game without properly uninstalling CBHC first! <https://wiiu.hacks.guide/#/uninstall-cbhc>
-        - Don’t move the DS Virtual Console game to a USB drive!
+    embed = discord.Embed(
+        title="CBHC Rules:",
+        description=cleandoc("""
+            - The DS game has to be legitimately installed from the eShop!
+            - Don’t format the system while CBHC is installed!
+            - Don’t delete the user account that bought the DS VC game!
+            - Don’t re-install the same game using WUP Installer or from the eShop!
+            - Don’t install Haxchi over CBHC! (You will not brick, but it will cause a boot-loop! Hold A when booting to access the Homebrew Launcher and uninstall CBHC.)
+            - Don’t uninstall the DS Virtual Console game without properly uninstalling CBHC first! <https://wiiu.hacks.guide/#/uninstall-cbhc>
+            - Don’t move the DS Virtual Console game to a USB drive!
         """)
     )
+    
+    await ctx.send(embed=embed)
 
 @bot.command()
 async def cfwuses(ctx, console):
