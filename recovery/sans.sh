49191edf2abfa84b66a7f27bc54314a88359522d
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-30 08:44:33 -0700

    did things for it to work on my pc

--- recovery/sans.sh
+++ recovery/sans.sh
@@ -2,5 +2,4 @@
 cd /home/lucas/Documents/Sans
 git reset #this is not a very elegant or perfect way to do it, but I suck at Git and also do not care
 git pull
-systemctl restart sans
-systemctl restart sansrecovery
+python3 sans.py
