71fe719a4401c1a68c2f61ccd47d7d7ad5954865
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-10-30 09:10:12 -0700

    ip address embed

--- sans.py
+++ sans.py
@@ -84,6 +84,24 @@ async def boot3dsx(ctx):
     )
     await ctx.send(embed=embed)
 
+@bot.command(aliases=["ipaddress"])
+async def ip(ctx):
+    '''shows how to get the ip address of your 3ds'''
+    embed = make_embed(
+        title="How to get your 3DS's IP Address (CFW)"
+        color=0xF5F5DC,
+        description=cleandoc("""
+            1. FBI
+            2. Remote Install
+            3. Receive URLs over the network
+            **Check your 3DSs IP (Homebrew)**
+            1. Open Homebrew Launcher
+            2. Press Y
+        """)
+    )
+    
+    await ctx.send(embed=embed)
+    
 @bot.command()
 async def listhelpers(ctx):
     '''Lists helpers in Internet Ivan's discord guild'''
