9dec2c1fb11b13df7b6969be07c6153be41580bd
Lazr <68634861+Lazr1026@users.noreply.github.com>
2020-11-27 03:18:18 -0800

    i swear to fucking god commas will kill me

--- sans.py
+++ sans.py
@@ -196,7 +196,7 @@ async def updateluma(ctx):
 	author="chenzw + lily",
 	color=0x000000,
 	thumbnail="https://avatars0.githubusercontent.com/u/5243259?s=400&v=4",
-	url="https://gist.github.com/lilyuwuu/3a7ba3dcd2476e6b5f4b6f66fa173bd6"
+	url="https://gist.github.com/lilyuwuu/3a7ba3dcd2476e6b5f4b6f66fa173bd6",
 	description="A guide for manually updating Luma3ds. This is necessary if you receive the \"Failed to apply 1 Firm patch(es)\" or \"pm\" errors",
     )
     await ctx.send(embed=embed)
