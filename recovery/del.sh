757b2d925281b6943030e443c7da747925edd697
Lazr <68634861+Lazr1026@users.noreply.github.com>
2022-06-04 12:36:01 -0700

    rewrite

--- recovery/del.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm /home/pi/ram/err #don't use this script unless you know it works, it will attempt to delete this file
--- recovery/sans.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd /home/lazr/Documents/Sans
-git reset #this is not a very elegant or perfect way to do it, but I suck at Git and also do not care
-git pull
-python3 sans.py
